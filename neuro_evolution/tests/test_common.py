9da7ae9887b2ec072d9d02282f2ec8caacd0c300
Marko Mlakar <markomlakar2@gmail.com>
2020-07-22 15:34:07 +0200

    Add sample Unit test
    * Needs fixing!

--- neuro_evolution/tests/test_common.py
+++ neuro_evolution/tests/test_common.py
@@ -2,13 +2,13 @@ import pytest
 
 from sklearn.utils.estimator_checks import check_estimator
 
-from neuro_evolution import NEATEstimator
-from neuro_evolution import TemplateClassifier
-from neuro_evolution import TemplateTransformer
+from neuro_evolution import NEATClassifier
+from neuro_evolution import NEATRegressor
 
 
 @pytest.mark.parametrize(
-    "Estimator", [NEATEstimator, TemplateTransformer, TemplateClassifier]
+    "Estimator", [NEATClassifier, NEATRegressor]
 )
 def test_all_estimators(Estimator):
+    # TODO: PASS the unit tests!!
     return check_estimator(Estimator)
